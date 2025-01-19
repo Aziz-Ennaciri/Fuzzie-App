07908ec1e9ee5d240ae84a7bde3eca07fcab4743
Aziz-Ennaciri <azizennaciri0@gmail.com>
2025-01-19 21:51:01 +0100

    update

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
@@ -10,6 +9,7 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip"
 import { menuOptions } from '@/lib/constant'
+import clsx from 'clsx'
 
 
 type Props = {}
@@ -20,8 +20,23 @@ const MenuOptions = (props: Props) => {
     <nav className="dark:bg-black h-screen overflow-scroll justify-between flex items-center flex-col gap-10 py-6 px-6">
         <div className="flex items-center justify-center flex-col gap-8">
             <Link className="flex font-bold flex-row" href="/">fuzzie</Link>
-            <TooltipProvider>{menuOptions.map(()=>
-            <div></div>)}
+            <TooltipProvider>{menuOptions.map((menuItem)=>
+            <ul key={menuItem.name}>
+              <Tooltip delayDuration={0}>
+                <TooltipTrigger>
+                  <li>
+                    <Link href={menuItem.href}
+                          className={clsx(
+                            'group h-8 w-8 flex items-center justify-center scale-[1.5] rounded-lg p-[3px] cursor-pointer',
+                            {'dark:bg-[#2F006B] bg-[#EEE0F]':pathName === menuItem.href}  
+                          )}>
+                            <menuItem.Component selected = {pathName === menuItem.href} />
+                    </Link>
+                  </li>
+                </TooltipTrigger>
+              </Tooltip>
+            </ul>
+            )}
             </TooltipProvider>
         </div>
     </nav>
